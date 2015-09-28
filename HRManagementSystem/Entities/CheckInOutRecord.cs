348e1b048fd22e169ef8a9533962652df9a0c967
hieupious <hieupious@ymail.com>
2015-09-28 11:08:43 +0700

    revert "Remove and Add some files"
    This reverts commit df0eec8f326e2a6aebf131d99647eba96185e536.

---
+++ HRManagementSystem/Entities/CheckInOutRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class CheckInOutRecord
+    {
+        public int UserId { get; set; }
+        public DateTime CheckTime { get; set; }
+        public UserInfo User { get; set; }
+    }
+}
