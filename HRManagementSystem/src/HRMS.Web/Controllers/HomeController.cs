a9ec5d74c5f0f31de35907fb592a276f951af956
hieupious <hieupious@ymail.com>
2015-10-20 08:56:54 +0700

    Remove unused references

--- HRManagementSystem/src/HRMS.Web/Controllers/HomeController.cs
+++ HRManagementSystem/src/HRMS.Web/Controllers/HomeController.cs
@@ -16,23 +16,12 @@ namespace HRMS.Web.Controllers
 
     public class HomeController : Controller
     {
-        private IOptions<AppSettings> appSetting;
-        private IOptions<ImportConfiguration> importConfig;
         private readonly ApplicationDbContext dbContext;
-        private readonly IImportDataService importDataService;
-        private readonly IDailyWorkingProcessService dailyWorkingProcess;
-        private readonly IMonthlyWorkingProcess monthlyWorkingProcess;
         private static int[] activeDepts = { 2, 3, 6, 7, 8, 9, 10 };
 
-        public HomeController(ApplicationDbContext dbContext,
-            IImportDataService importDataService, IDailyWorkingProcessService dailyWorkingProcess,
-            IOptions<ImportConfiguration> importConfig, IMonthlyWorkingProcess monthlyWorkingProcess)
+        public HomeController(ApplicationDbContext dbContext)
         {
             this.dbContext = dbContext;
-            this.importDataService = importDataService;
-            this.dailyWorkingProcess = dailyWorkingProcess;
-            this.monthlyWorkingProcess = monthlyWorkingProcess;
-            this.importConfig = importConfig;
         }
         [Authorize(Roles = "NormalUser,Manager,Administrator,HRGroup")]
         public IActionResult Index(string id)
